9a879bbd95b2359aff94219851490743a1d1a942
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-19 20:18:10 +0000

    Exponentsloop

--- guessRightNumber.cpp
+++ guessRightNumber.cpp
@@ -3,18 +3,16 @@
 // Created On March 2022
 // This is a loop adder
 
-
 #include <iostream>
-
+#include <string>
 
 
 int main() {
     // this function is a loop adder
     std::string integerAsNumber;
     int number;
-    int total = 1;
+    int total = 0;
     int loopNumber = 0;
-
 
     // input
     std::cout << "Enter your number!: ";
@@ -24,14 +22,17 @@ int main() {
     // process & output
     try {
         number = std::stoi(integerAsNumber);
-    do  {
-        loopNumber = loopNumber + 1;
-        total = total * loopNumber;
-     } while (loopNumber < number);
-        std::cout << "The sum of all the positive numbers from 1 to "
-        << number << " is " << total << std::endl;
+        if (number >= 0) {
+            for (int loopNumber = 0; loopNumber < number + 1; loopNumber++) {
+                total = loopNumber * loopNumber;
+                std::cout << "The sum of all the positive numbers from 1 to "
+                << number << " is " << total << std::endl;
+            }
+        } else {
+            std::cout << "Not a valid number!" << std::endl;
+        }
     } catch (std::invalid_argument) {
         std::cout << "Invalid Input.";
-    }
+        }
     std::cout << "\nDone." << std::endl;
 }
